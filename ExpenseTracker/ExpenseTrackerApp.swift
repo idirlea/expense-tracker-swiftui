e2a488e78b07111a78bc70fa339623a573d1492d
Ionut Andrei Dirlea <ionut.dirlea@gmail.com>
2021-02-13 14:50:05 +0200

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Ionut Dirlea on 13.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
